18b2144633677a6fbe1a45fb6b5c57eeb22ca696
Khadidja <khadija.aitsiali@gmail.com>
2022-03-08 18:15:05 +0100

    navbar done with media queries

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,10 @@ import logo from '../../assets/logo_small.png';
 const Menu = () => (
   <>
     <ul>
-      <li><a href="#">About <RiArrowLeftSFill/></a></li>
-      <li><a href="#interior">Interior <RiArrowLeftSFill/></a></li>
-      <li> <a href="#contact">Contact <RiArrowLeftSFill/></a></li>
-      <li> <a href="#testimonial">Testimonial <RiArrowLeftSFill/></a></li>    
+      <li><a href="#">About </a></li>
+      <li><a href="#interior">Interior </a></li>
+      <li> <a href="#contact">Contact </a></li>
+      <li> <a href="#testimonial">Testimonial </a></li>    
     </ul>
   </>
 )
@@ -19,21 +19,18 @@ const Navbar = () => {
 
   return (
 
-    <div className="navbar bg-color">
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <a href="#"><img src={logo} alt="Logo Levyna" /></a>
-        </div>  
-        <div className="navbar-links">
-          <Menu />
-        </div>  
-        <div className="navbar-social-media">
-          <a href="http://facebook.com" target="_blank" rel="noopener noreferrer"><RiFacebookCircleFill/></a>
-          <a href="http://instagram.com" target="_blank" rel="noopener noreferrer"><RiInstagramLine/></a>
+    <header className="navbar bg-color">
+      <a href="#"><img src={logo} alt="Logo Levyna" /></a>
+        <nav className="navbar-container">
+            <Menu />
+        </nav>
+        <div className='navbar-social-media'>
+          <a className='social-media' href="http://facebook.com" target="_blank" rel="noopener noreferrer"><RiFacebookCircleFill/></a>
+          <a className='social-media' href="http://instagram.com" target="_blank" rel="noopener noreferrer"><RiInstagramLine/></a>
           <button className='navbar-button' type='button'><a href="#contact"></a>Get Started</button>
-        </div>   
-      </div>
-      <div className="navbar-menu">
+        </div>
+
+      <div className="navbar-menu">   
         {toggleMenu
           ? <RiCloseFill color='#fff' size={25} onClick={() => setToggleMenu(false)} />
           : <RiMenu4Fill color='#fff' size={25} onClick={() => setToggleMenu(true)} />
@@ -53,8 +50,7 @@ const Navbar = () => {
           </div>
         )}
       </div>
-    </div>
-
+    </header>
   )
 }
 
