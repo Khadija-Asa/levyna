4a6446f29c52ad8659bc5b36314c48f4090e4502
Khadidja <khadija.aitsiali@gmail.com>
2022-03-17 17:08:24 +0100

    files added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import './App.css'
+import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import Header from "./containers/header/Header";
-import Slider from "./components/slider/Slider";
+import Body from "./containers/body/Body";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <Slider />
+      <Body />
     </div>
-    
   );
 }
 
--- src/containers/body/Body.jsx
+++ src/containers/body/Body.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './body.css';
+import uber from '../../assets/uber.png';
+import launchdarkly from '../../assets/launchdarkly.png';
+import intercom from '../../assets/intercom.png'
+import squarespace from '../../assets/squarespace.png';
+import hashicorp from '../../assets/hashicorp.png';
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <section className="body">
+      
+      <div className="body-partners">
+        <img src={ uber } alt="Logo partenaire Uber" />
+        <img src={ launchdarkly } alt="Logo partenaire LaunchDarkly" />
+        <img src={ intercom } alt="Logo partenaire Intercom" />
+        <img src={ squarespace } alt="Logo partenaire SquareSpace" />
+        <img className='partners-hashicorp' src={ hashicorp } alt="Logo partenaire HashiCorp" />
+      </div>
+
+    </section>
   )
 }
 
