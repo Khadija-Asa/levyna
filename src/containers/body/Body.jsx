1abeb195ca84278a4638065352ac6f85fba3d513
Khadidja <khadija.aitsiali@gmail.com>
2022-03-17 18:44:05 +0100

    presentation done with mq

--- src/containers/body/Body.jsx
+++ src/containers/body/Body.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './body.css';
 import uber from '../../assets/uber.png';
 import launchdarkly from '../../assets/launchdarkly.png';
-import intercom from '../../assets/intercom.png'
+import intercom from '../../assets/intercom.png';
 import squarespace from '../../assets/squarespace.png';
 import hashicorp from '../../assets/hashicorp.png';
+import presentation from '../../assets/bg-salon.jpg';
 
 const Body = () => {
   return (
@@ -16,6 +17,23 @@ const Body = () => {
         <img src={ intercom } alt="Logo partenaire Intercom" />
         <img src={ squarespace } alt="Logo partenaire SquareSpace" />
         <img className='partners-hashicorp' src={ hashicorp } alt="Logo partenaire HashiCorp" />
+      </div>
+
+      <div className="body-presentation">
+        <img src={ presentation } alt="Image de présentation" />
+        <br /> 
+        <br />
+        <div className="presentation-text">
+          <h2>The <span className="highlight">best interior</span> comes from Levyna</h2> 
+          <br />
+          <p className="presentation-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled it to make a type specimen book.
+          </p> 
+          <br />
+          <a href="#interior"><button className="presentation-button">See our interior</button></a>
+        </div>
       </div>
 
     </section>
